e771361aa4e2dc08097851ee2556c20136a64dec
pohjie <0xArlet@gmail.com>
2023-02-08 19:12:31 +0800

    deploy to gh-pages

--- src/index.js
+++ src/index.js
@@ -4,16 +4,9 @@ import './index.css';
 import App from './App';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-import BrowserRouter from 'react-router-dom/BrowserRouter'
-
-ReactDOM.render((
-   <BrowserRouter basename={process.env.PUBLIC_URL}>
-     <App />
-   </BrowserRouter>
-));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
