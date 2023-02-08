ab1c77f429a823f789efe4a6de4f99ed033216cd
pohjie <0xArlet@gmail.com>
2023-02-08 18:52:31 +0800

    Rename class for modularity

--- src/components/display/EdExpRow.js
+++ src/components/display/EdExpRow.js
@@ -9,8 +9,8 @@ class EdExpRow extends Component {
     return (
       <div id={this.props.id} className='ed-exp-details'>
         <div className='ed-exp-entry-left'>
-          <div className='school'>{this.props.schoolName}</div>
-          <div className='title-of-study'>{this.props.titleOfStudy}</div>
+          <div className='entry-name'>{this.props.schoolName}</div>
+          <div className='title'>{this.props.titleOfStudy}</div>
         </div>
         <div className='date-section'>{this.props.dateFrom} to {this.props.dateTo}</div>
       </div>
